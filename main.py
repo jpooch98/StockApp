2717c9098a05204a339e1b55b77adccb3f4b2129
James <45608681+jpooch98@users.noreply.github.com>
2023-01-08 16:45:50 -0500

    Removed the auto run, would need to restructure

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-import find, window, CSVManipulator, price
-
-# Gets stock price from Google
-DOW_price = price.getDowPrice()
-SP_price = price.getSPPrice()
+import find, window, CSVManipulator, price, schedule, time
 
 
-# Converting the output to a string
-DOW_price = str(DOW_price)
-SP_price = str(SP_price)
-
-# Regex to get just the price 
-SP_price = find.regex(SP_price)
-DOW_price = find.regex(DOW_price)
+def priceFunc():
+    # Gets stock price from Google
+    DOW_price = price.getDowPrice()
+    SP_price = price.getSPPrice()
 
 
-# Calls our custom function that creates a window that displays the current prices for the stocks
-window.show(SP_price, DOW_price)
+    # Converting the output to a string
+    DOW_price = str(DOW_price)
+    SP_price = str(SP_price)
 
-# A function that puts the values in a csv and prints the values to the terminal
-CSVManipulator.addPrice("S&P", SP_price)
-CSVManipulator.addPrice("DOW", DOW_price)
+    # Regex to get just the price 
+    SP_price = find.regex(SP_price)
+    DOW_price = find.regex(DOW_price)
 
-CSVManipulator.printPrices()
+
+    # Calls our custom function that creates a window that displays the current prices for the stocks
+    window.show(SP_price, DOW_price)
+
+    # A function that puts the values in a csv and prints the values to the terminal
+    CSVManipulator.addPrice("S&P", SP_price)
+    CSVManipulator.addPrice("DOW", DOW_price)
+    CSVManipulator.printPrices()
+
+
+
+# Runs every minutes but pauses until window is removed
+priceFunc()
